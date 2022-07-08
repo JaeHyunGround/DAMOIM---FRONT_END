cb938dc669fd1fb88b727b3cd61cf44c2ec5af52
Miniling <bsm3925@naver.com>
2022-07-09 00:25:38 +0900

    header

--- damoim_front/src/component/header.js
+++ damoim_front/src/component/header.js
@@ -4,17 +4,32 @@ import { Link } from 'react-router-dom';
 function Header() {
     return (
         <>
-            <section className='headLeft'>
-                <span>
-                    <Link to="/">
-                        <img className='logo' src='image/Kit_Logo.png' />
-                    </Link>
-                </span>
+            <section className='headTop'>
+                <div className='headLeft'>
+                    <span>
+                        <Link to="/">
+                            <img className='logo' src='image/Kit_Logo.png' />
+                        </Link>
+                    </span>
+                </div>
+
+                <div className='headCenter'>
+                    <span>
+                        <a>DAMOIM</a>
+                    </span>
+                </div>
+
+                <div className='headRight'>
+                    <span><Link to="/login">로그인</Link></span>
+                    <span><Link to="/signUp">회원가입</Link></span>
+                </div>
             </section>
 
-            <section className='headRight'>
-                <span><Link to="/login">로그인</Link></span>
-                <span><Link to="/signUp">회원가입</Link></span>
+            <section className='headBottom'>
+                <a>분류1</a>
+                <a>분류2</a>
+                <a>분류3</a>
+                <a>분류4</a>
             </section>
         </>
     );
