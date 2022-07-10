75a2352697d58db0c7db70b4486d164a24386e01
so0y <jenny1717@naver.com>
2022-07-11 08:38:49 +0900

    동아리 카드 수정

--- damoim_front/src/clubData.js
+++ damoim_front/src/clubData.js
@@ -1,34 +1,161 @@
 const clubData = [
     {
         type: '예술',
-        name: '멋쟁이 예술',
-        url: 'club/art',
+        name: 'C.O.K',
+        url: 'club/art1',
+        img: '/image/C.O.K.png'
+    },
+    {
+        type: '예술',
+        name: '페이즈',
+        url: 'club/art2',
+        img: '/image/faze.png'
+    },
+    {
+        type: '예술',
+        name: '공데쌩',
+        url: 'club/art3',
+        img: '/image/godaessang.png'
+    },
+    {
+        type: '예술',
+        name: '금오사진예술연구회',
+        url: 'club/art4',
+        img: '/image/kumohphoto.png'
+    },
+    {
+        type: '예술',
+        name: '소리샘',
+        url: 'club/art5',
+        img: '/image/sorisam.jpg'
+    },
+    {
+        type: '예술',
+        name: '타니말라',
+        url: 'club/art/5',
+        img: '/image/tanimalla.png'
+    },
+    {
+        type: '예술',
+        name: 'Hello kit',
+        url: 'club/art/5',
+        img: '/image/Hello kit.png'
+    },
+    {
+        type: '예술',
+        name: '모아',
+        url: 'club/art/5',
+        img: '/image/MOA.png'
     },
     {
         type: '스포츠',
-        name: '멋쟁이 운동',
+        name: 'COBRA',
         url: 'club/sport1',
+        img: '/image/cobra.png'
     },
     {
         type: '스포츠',
-        name: '멋쟁이 운동2',
+        name: 'Golden Crows',
         url: 'club/sport2',
+        img: '/image/Golden Crows.png'
+    },
+    {
+        type: '스포츠',
+        name: '한일탁구회',
+        url: 'club/sport3',
+        img: '/image/hanarl.png'
+    },
+    {
+        type: '스포츠',
+        name: '하이클리어',
+        url: 'club/sport3',
+        img: '/image/highclear.png'
+    },
+    {
+        type: '스포츠',
+        name: '금오태극회',
+        url: 'club/sport4',
+        img: '/image/kumohtaeguk.png'
+    },
+    {
+        type: '스포츠',
+        name: '태양농구회',
+        url: 'club/sport5',
+        img: '/image/taeyang.png'
+    },
+    {
+        type: '스포츠',
+        name: 'KumohtennisCKUB',
+        url: 'club/sport6',
+        img: '/image/tennis.png'
+    },
+    {
+        type: '스포츠',
+        name: 'SKIT',
+        url: 'club/sport7',
+        img: '/image/SKIT.png'
     },
     {
         type: '여행',
-        name: '멋쟁이 여행',
-        url: 'club/travel',
+        name: '망고',
+        url: 'club/travel1',
+        img: '/image/mango.png'
     },
     {
-        type: '종교',
-        name: '멋쟁이 종교',
-        url: 'club/religion',
+        type: '종교 & 봉사',
+        name: 'CCC',
+        url: 'club/religion1',
+        img: '/image/CCC.png'
+    },
+    {
+        type: '종교 & 봉사',
+        name: '두모사',
+        url: 'club/religion2',
+        img: '/image/dumosa.png'
+    },
+    {
+        type: '종교 & 봉사',
+        name: '미담장학회',
+        url: 'club/religion3',
+        img: '/image/midam.png'
+    },
+    {
+        type: '종교 & 봉사',
+        name: '나눔',
+        url: 'club/religion4',
+        img: '/image/nanum.jpeg'
+    },
+    {
+        type: '종교 & 봉사',
+        name: 'With Us',
+        url: 'club/religion8',
+        img: '/image/with us.png'
     },
     {
         type: '스터디',
-        name: '멋쟁이 공부',
+        name: 'Auto-Mania',
         url: 'club/study',
+        img: '/image/Auto-Mania.png'
     },
+    {
+        type: '스터디',
+        name: 'ECRC',
+        url: 'club/study',
+        img: '/image/ECRC.png'
+    },
+    {
+        type: '스터디',
+        name: '묵향',
+        url: 'club/study',
+        img: '/image/mukhyang.png'
+    },
+    {
+        type: '스터디',
+        name: '거북신선화',
+        url: 'club/study',
+        img: '/image/turtle.png'
+    }
+
 ]
 
 export function getData() {
